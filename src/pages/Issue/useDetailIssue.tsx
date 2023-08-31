33854bfefa9adee8471baaaf940c13eccfe82dd9
Park jinyoung <asd029600@gmail.com>
2023-08-31 11:23:04 +0900

    fix : 이슈 상세 정보 페이지에서 잘못된 이슈 넘버로 요청 시 에러 화면 나오지 않도록 처리

--- src/pages/Issue/useDetailIssue.tsx
+++ src/pages/Issue/useDetailIssue.tsx
@@ -1,14 +1,23 @@
 import { useEffect, useState } from "react";
 import { getDetailedIssue, IResIssue } from "../../api/issues";
 
-export const useDetailIssue = (issueNumber: number) => {
+export const useDetailIssue = (issueNumber: string | number) => {
   const [detailIssue, setDetailIssue] = useState<IResIssue>();
+  const [isDetailIssueLoading, setIsDetailIssueLoading] =
+    useState<boolean>(false);
 
   useEffect(() => {
+    setIsDetailIssueLoading(true);
     getDetailedIssue(issueNumber).then((data: IResIssue) => {
       setDetailIssue(data);
+      setIsDetailIssueLoading(false);
     });
   }, []);
 
-  return { detailIssue, setDetailIssue };
+  return {
+    detailIssue,
+    setDetailIssue,
+    isDetailIssueLoading,
+    setIsDetailIssueLoading,
+  };
 };
