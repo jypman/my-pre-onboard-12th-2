820f51246ba150d34f497a00ecc057897c3f581e
Park jinyoung <asd029600@gmail.com>
2023-09-14 18:40:13 +0900

    remove : remark-gfm 삭제

--- src/components/MarkDown.tsx
+++ src/components/MarkDown.tsx
@@ -1,11 +1,10 @@
 import React from "react";
 import ReactMarkdown from "react-markdown";
-import remarkGfm from "remark-gfm";
 
 interface MarkDownProps {
   content: string;
 }
 
 export const MarkDown = ({ content }: MarkDownProps) => {
-  return <ReactMarkdown children={content} remarkPlugins={[remarkGfm]} />;
+  return <ReactMarkdown children={content} />;
 };
